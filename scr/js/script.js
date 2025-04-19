34aae2fc02930803be42c4b10545510ec1458aae
MrBistr <163679491+MrBistr@users.noreply.github.com>
2025-04-19 13:14:38 +0300

    Delete scr directory

--- scr/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Social Map App started');
-    // Initialize canvas
-    const canvas = document.getElementById('canvas');
-    // TODO: Add functionality
-});
